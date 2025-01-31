07569e6b2e85f7b3896ae41a125ef0909978c9cd
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-01-31 19:41:50 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+console.log("backend starting");
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+// GET method route
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('youtube dekhna hh')
+})
+  
+// POST method route
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+})
